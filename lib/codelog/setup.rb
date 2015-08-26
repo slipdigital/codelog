02b588d6d40ea6b444aa928e34e21381fef2b66e
Douglas Mills <slipdigital@gmail.com>
2015-08-26 07:29:22 +0100

    minor update

--- lib/codelog/setup.rb
+++ lib/codelog/setup.rb
@@ -1,8 +1,9 @@
 require 'fileutils'
+require 'yaml'
 
 class CodeLog::Setup
 
-  def self.scan(path, depth=1)
+  def self.scan(path, depth=1, period='1 week ago')
 
     # verify that the settings folder and file exist
     verify_settings_folder
@@ -12,7 +13,7 @@ class CodeLog::Setup
     directories = Dir.glob('*').select { |f| File.directory? f }
 
     directories.each do |dir|
-      print "checking : #{path}#{dir} "
+
       Dir.chdir("#{path}#{dir}")
 
       require 'open3'
@@ -23,39 +24,41 @@ class CodeLog::Setup
       stderr.close
       exit_code = wait_thr.value
 
-      print((exit_code == 0 ? ' ... YES' : ' ... NO') + "\n\n")
+      if exit_code
+        print "#{path}#{dir} is a valid repo : \n\n"
+        store_repo_path("#{path}#{dir}")
+      end
 
       commits = []
       if exit_code == 0
         begin
-          commits << history("#{path}#{dir}")
+          commits.push(*history("#{path}#{dir}", period))
         rescue Git::GitExecuteError
           # result = 'Something went wrong with this repo ... '
         end
       end
 
 
+      commits.each do |commit_obj|
+        g = Git.open(commit_obj.instance_variable_get('@base').instance_variable_get('@working_directory').path)
+        commit = g.gcommit(commit_obj.sha)
 
-      commits.each do |commit_ref|
-        g = Git.open("#{path}#{dir}")
-        commit = g.gcommit(commit_ref)
-
-        print commit.author.name
-        print commit.author.email
-        print commit.author.date.strftime("%m-%d-%y")
-        print commit.committer.name
-        print commit.date.strftime("%m-%d-%y")
+        print commit.committer.name + ' : '
+        print commit.author.email + ' : '
+        print commit.author.date.strftime("%m-%d-%y %H:%M:%S")
+        print "\n\n"
+        print commit.message
+        print "\n\n"
+        print commit.to_yaml
       end
       # print result
 
     end
   end
 
-  def self.history(path)
+  def self.history(path, period)
     g = Git.open(path)
-    print g.log.since("\n\n")
-    print g.log.since('1 months ago')
-    print g.log.since("\n\n")
+    g.log.since(period)
   end
 
   private
@@ -63,9 +66,12 @@ class CodeLog::Setup
   def self.verify_settings_folder
     # check folder not exist
     pwd = Dir.pwd
-    print "Verifying file system setup.\n"
-    print "---------------------------------------------------\n\n"
-    print "checking directory : '#{ENV['HOME']+'/.codelog'}' exists.\n"
+
+    unless Dir.exists?(ENV['HOME']+'/.codelog') || !Dir.exists?(ENV['HOME']+'/.codelog/repositories.yml')
+      print "Verifying file system setup.\n"
+      print "---------------------------------------------------\n\n"
+      print "checking directory : '#{ENV['HOME']+'/.codelog'}' exists.\n"
+    end
 
 
     unless Dir.exists?(ENV['HOME']+'/.codelog')
@@ -84,6 +90,10 @@ class CodeLog::Setup
 
     print "OK, done! Good to go\n\n"
     Dir.chdir pwd
+  end
+
+  def self.store_repo_path(path)
+
   end
 
 end
